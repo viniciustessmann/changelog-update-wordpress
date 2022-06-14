4f78289cd7ed81de1498f20f03bf30d9cc233b7d
Vinícius Tessmann <vinicius.tessmann@melhorenvio.com>
2022-06-14 10:39:27 -0300

    feat: create new feature test

--- .github/update-changelog.sh
+++ .github/update-changelog.sh
@@ -2,11 +2,15 @@ git fetch
 
 last_version=$(git tag --sort=committerdate | tail -1)
 
-versions=$(git tag --sort=committerdate | tail -2)
+penultimate_version=$(git describe --abbrev=0 --match="v*" --tags HEAD~)
 
 release=${last_version#?}
 
-tags=$(echo $versions | tr ' '  '..')
+echo $last_version
+
+echo $penultimate_version
+
+tags=$penultimate_version..$last_version
 
 echo $(> CHANGELOG.md);
 
@@ -16,10 +20,8 @@ echo "$x" >> CHANGELOG.md
 x=$(echo "= ${release} =")
 echo "$x" >> CHANGELOG.md
 
-pull_requests=$(git log v5.0.0..v6.0.0 --merges  --format="* %b" >> CHANGELOG.md)
+pull_requests=$(git log $tags --merges  --format="* %b" >> CHANGELOG.md)
 
 delimiter="Merge pull request #"
-
-git log v5.0.0..v6.0.0 --merges  --format="* %b"
 
 echo $pull_requests
